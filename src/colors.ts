1680d72d343a28ae6eeb60e8d82733e225f61b14
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-08-17 20:41:39 -0300

    `Holy` - Things are really downgraded, need to change everything haha

--- src/colors.ts
+++ src/colors.ts
@@ -10,4 +10,15 @@ const COLORS = {
   Black4: '#454652',
 };
 
+const colors = {
+  dark: {
+    bg: '#16161A',
+    bgLight: '#242629',
+    primary: '#7F5AF0',
+    secondary: '#2CB67D',
+    white: '#FFFFFE',
+    gray: '#94A1B2',
+  },
+};
+
 export default COLORS;
